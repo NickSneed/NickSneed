8de098603404047aeae4eecbf7e3c94ebd252d8f
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2024-12-28 20:30:29 -0600

    remove webgl test

--- src/modules/stars.js
+++ src/modules/stars.js
@@ -1,7 +1,5 @@
 import * as THREE from 'three';
 
-var webgltest = true;
-
 var scene = new THREE.Scene(),
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10000),
     renderer,
@@ -20,11 +18,9 @@ function render() {
 
 function setupScene() {
 
-    if (webgltest) {
-        renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    } else {
-        renderer = new THREE.CanvasRenderer();
-    }
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x000000, 0);
@@ -34,35 +30,26 @@ function setupScene() {
     camera.position.x = cameraDistance * Math.cos(x);
     camera.position.z = cameraDistance * Math.sin(x);
 
-    if (webgltest) {
-        var ambientLight = new THREE.AmbientLight(0xffffff),
-            lights = [];
-        scene.add(ambientLight);
 
-
+    var ambientLight = new THREE.AmbientLight(0xffffff),
         lights = [];
-        lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-        lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-        lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+    scene.add(ambientLight);
 
-        lights[0].position.set(0, 200, 0);
-        lights[1].position.set(100, 200, 100);
-        lights[2].position.set(-100, -200, -100);
 
-        scene.add(lights[0]);
+    lights = [];
+    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
 
-    }
+    lights[0].position.set(0, 200, 0);
+    lights[1].position.set(100, 200, 100);
+    lights[2].position.set(-100, -200, -100);
 
+    scene.add(lights[0]);
 }
 
 function addElements() {
-    var PI2 = Math.PI * 2,
-        program = function (context) {
-            context.beginPath();
-            context.arc(0, 0, 0.2, 0, PI2, true);
-            context.fill();
-        },
-        group = new THREE.Group(),
+    var group = new THREE.Group(),
         i,
         material,
         particle,
@@ -80,21 +67,13 @@ function addElements() {
             color = 0x72f5ff;
         }
 
-        if (webgltest) {
-            material = new THREE.MeshPhongMaterial({
-                color: color,
-                emissive: 0x072534,
-                side: THREE.DoubleSide,
-                shading: THREE.FlatShading
-            });
-            particle = new THREE.Mesh(geometry, material);
-        } else {
-            material = new THREE.SpriteCanvasMaterial({
-                color: color,
-                program: program
-            });
-            particle = new THREE.Sprite(material);
-        }
+        material = new THREE.MeshPhongMaterial({
+            color: color,
+            emissive: 0x072534,
+            side: THREE.DoubleSide,
+            shading: THREE.FlatShading
+        });
+        particle = new THREE.Mesh(geometry, material);
 
         particle.position.x = Math.random() * 2000 - 1000;
         particle.position.y = Math.random() * 2000 - 1000;
@@ -103,11 +82,8 @@ function addElements() {
         if (scale > 8) {
             scale = Math.random() * 20;
         }
-        if (webgltest) {
-            particle.scale.set(scale, scale, scale);
-        } else {
-            particle.scale.x = particle.scale.y = scale;
-        }
+
+        particle.scale.set(scale, scale, scale);
         group.add(particle);
     }
 
