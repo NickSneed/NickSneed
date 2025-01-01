6bf808f8f347f6626cc62184fed5de25a88cc18c
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2024-12-31 18:50:50 -0600

    restructured folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/js/main.js',
     mode: 'production',
     output: {
-        filename: 'main.js',
+        filename: 'js/main.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/index.html', to: 'index.html' },
+                { from: 'src/img', to: 'img' }
+            ]
+        })
+    ]
 };
