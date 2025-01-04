ba974a6b6f2696a4818eb1f1cf88e095a719ced8
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2025-01-04 15:30:00 -0600

    renamed app.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-    entry: './src/js/app.js',
+    entry: './src/js/App.js',
     mode: 'production',
     output: {
         filename: 'app.js',
