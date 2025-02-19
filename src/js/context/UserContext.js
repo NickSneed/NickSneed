9657f0c054dfa8d4e0440d7402438f024b450f48
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2025-02-18 19:48:41 -0600

    added validation to userID

--- src/js/context/UserContext.js
+++ src/js/context/UserContext.js
@@ -10,18 +10,63 @@ const generateUserID = () => {
     return userID;
 };
 
+const SECURITY_CONFIG = {
+    MAX_ID_LENGTH: 9, // 'U' + 8 digits
+    ALLOWED_CHARS: /^[U\d]+$/
+};
+
+// Add validation function
+const isValidUserID = (id) => {
+
+    // Check if the ID is a string and meets length and character requirements
+    if (!id || typeof id !== 'string') return false;
+    if (id.length > SECURITY_CONFIG.MAX_ID_LENGTH) return false;
+    if (!SECURITY_CONFIG.ALLOWED_CHARS.test(id)) return false;
+
+    // Check if the ID starts with 'U' and is followed by 8 digits
+    const userIDPattern = /^U\d{8}$/;
+    return userIDPattern.test(id);
+};
+
+// Utility functions to get value in localStorage with error handling
+const getFromStorage = (key) => {
+    try {
+        let item = localStorage.getItem(key);
+        if (item) {
+            item = decodeURIComponent(item);
+        }
+        return isValidUserID(item) ? item : generateUserID();
+    } catch (e) {
+        console.error('Error reading from localStorage:', e);
+        return generateUserID();
+    }
+};
+
+// Function to set a value in localStorage with error handling
+const setToStorage = (key, value) => {
+    try {
+        if (isValidUserID(value)){
+            localStorage.setItem(key, encodeURIComponent(value));
+        } else {
+            localStorage.setItem(key, generateUserID());
+        }
+    } catch (e) {
+        console.error('Error writing to localStorage:', e);
+    }
+};
+
 export const UserProvider = ({ children }) => {
 
     // Check localStorage on initial render and return the userID
     const [userID] = useState(() => {
-        const savedUserID = localStorage.getItem('userID');
-        return savedUserID || generateUserID();
+        const savedUserID = getFromStorage('userID');
+        return savedUserID;
     });
 
-    
     // Update localStorage when userID changes
     useEffect(() => {
-        localStorage.setItem('userID', userID);
+        // Validate before saving to localStorage
+        setToStorage('userID', userID);
     }, [userID]);
 
     return (
