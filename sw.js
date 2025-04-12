469c1513bc0ded12ef2be5da25cb084f85b373a5
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2025-04-12 08:35:19 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-var GHPATH="/",APP_PREFIX="nspwa_",VERSION="version_00",URLS=[`${GHPATH}`,`${GHPATH}index.html`,`${GHPATH}app.js`];
+const CACHE_NAME="nick-sneed-cache-v1",urlsToCache=["/","/index.html","/manifest.json"];self.addEventListener("install",(e=>{e.waitUntil(caches.open(CACHE_NAME).then((e=>e.addAll(urlsToCache))))})),self.addEventListener("fetch",(e=>{e.respondWith(caches.match(e.request).then((t=>t||fetch(e.request))))}));
