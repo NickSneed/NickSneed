fb72340cd5e40e046f6a703a5165a022d6d5bad9
Nick Sneed <57041223+NickSneed@users.noreply.github.com>
2025-10-10 19:29:00 -0500

    style adjustments

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -20,45 +20,55 @@ const Main = () => {
     // Component HTML
     const html = (
         <div className={styles.main}>
-            <div className={styles.mainimg}>
-                <Img
-                    src={familyImg}
-                    srcSmall={familyImgSmall}
-                    alt="My family"
-                    percent={100}
+            <div className={`${styles.full} ${styles.introContainer}`}>
+                <div className={styles.mainimg}>
+                    <Img
+                        src={familyImg}
+                        srcSmall={familyImgSmall}
+                        alt="My family"
+                        percent={100}
+                    />
+                </div>
+                <div className={styles.maincopy}>
+                    <h2>{str('mainIntro1', true)}</h2>
+                    <p>{str('mainIntro2', true)}</p>
+                    <p>{str('mainIntro3', true)}</p>
+                </div>
+            </div>
+            <div>
+                <SocialTile
+                    img={headerInsta}
+                    title={str('socialTileInstaAlt')}
+                    text={str('socialTileInsta', true, [userID])}
+                    link="https://www.instagram.com/bobrumbly/"
+                    alt={str('socialTileInstaAlt')}
+                    linkTxt={str('socialTileInstaBt')}
                 />
             </div>
-            <div className={styles.maincopy}>
-                <h2>{str('mainIntro1', true)}</h2>
-                <p>{str('mainIntro2', true)}</p>
-                <p>{str('mainIntro3', true)}</p>
+            <div>
+                <SocialTile
+                    img={headerTumblr}
+                    title={str('socialTileTumblrAlt')}
+                    text={str('socialTileTumblr', true, [userID])}
+                    link="https://bobrumbly.com"
+                    alt={str('socialTileTumblrAlt')}
+                    linkTxt={str('socialTileTumblrBt')}
+                />
             </div>
-            <SocialTile
-                img={headerInsta}
-                title={str('socialTileInstaAlt')}
-                text={str('socialTileInsta', true, [userID])}
-                link="https://www.instagram.com/bobrumbly/"
-                alt={str('socialTileInstaAlt')}
-                linkTxt={str('socialTileInstaBt')}
-            />
-            <SocialTile
-                img={headerTumblr}
-                title={str('socialTileTumblrAlt')}
-                text={str('socialTileTumblr', true, [userID])}
-                link="https://bobrumbly.com"
-                alt={str('socialTileTumblrAlt')}
-                linkTxt={str('socialTileTumblrBt')}
-            />
-            <SocialTile
-                img={headerGitHub}
-                title={str('socialTileGitHubAlt')}
-                text={str('socialTileGitHub', true, [userID])}
-                link="https://github.com/NickSneed/"
-                alt={str('socialTileGitHubAlt')}
-                linkTxt={str('socialTileGitHubBt')}
-            />
-            <hr />
-            <SpaceLaunches />
+            <div>
+                <SocialTile
+                    img={headerGitHub}
+                    title={str('socialTileGitHubAlt')}
+                    text={str('socialTileGitHub', true, [userID])}
+                    link="https://github.com/NickSneed/"
+                    alt={str('socialTileGitHubAlt')}
+                    linkTxt={str('socialTileGitHubBt')}
+                />
+            </div>
+            <div className={styles.full}>
+                <hr />
+                <SpaceLaunches />
+            </div>
         </div>
     );
 
